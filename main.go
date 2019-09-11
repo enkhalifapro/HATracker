b5e95385ce1a791fde9a0de059ada3649832db30
ayman <enkhalifapro@gmail.com>
2019-09-11 19:36:53 +0200

    finish router config

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+  "github.com/labstack/echo"
+  "net/http"
+)
+
+type Issue struct {
+  ID    int
+  Title string
+}
 
 func main() {
-  fmt.Println("Getting started")
+  r := echo.New()
+
+  r.GET("/hager/ahmed", func(c echo.Context) error {
+    myIssue := &Issue{ID: 1, Title: "My first issue"}
+    return c.JSON(http.StatusOK, myIssue)
+  })
+
+  r.Start("localhost:7071")
 }
